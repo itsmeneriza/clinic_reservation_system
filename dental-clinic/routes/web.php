5c296c1aab0e16fbb1279a904160d04323948868
itsmeneriza <saldoneriza19@gmail.com>
2025-03-28 21:56:19 +0000

    updated web routes for temporary public access

--- dental-clinic/routes/web.php
+++ dental-clinic/routes/web.php
@@ -1,13 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+//temporary access for the reservation controller
+Route::resource('reservations', ReservationController::class);
+
+/*Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -18,3 +22,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+*/
